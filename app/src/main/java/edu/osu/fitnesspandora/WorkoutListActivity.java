a70fabf60af99ccfea3cc69bfc002a06835f83e6
Don Herwig <herwig.8@osu.edu>
2016-03-23 04:50:30 -0400

    small changes to get workoutList working

--- app/src/main/java/edu/osu/fitnesspandora/WorkoutListActivity.java
+++ app/src/main/java/edu/osu/fitnesspandora/WorkoutListActivity.java
@@ -10,12 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 //this will load the workout names in a listview and will be dynamic based on what we have in workout lab
+//will have to update it a bit to get the view right
 /* I will also add click listeners that will launch intents and pass the data about which one
 was clicked to the exercise activity so it knows which category of exercises to choose from
  */
 public class WorkoutListActivity extends ListActivity {
 
-    WorkoutLab test = new WorkoutLab();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +26,16 @@ public class WorkoutListActivity extends ListActivity {
 
     //this method gets the workout names from the Workout Lab and is used for the ListAdapter
     private static String[] getWorkoutNames(){
-        WorkoutLab workouts = new WorkoutLab();
+        WorkoutLab workoutLab = WorkoutLab.get();
+        ArrayList<Workout> workouts = workoutLab.getWorkouts();
 
-        //for some reason getWorkouts.size is size 0, it isn't returning anything
-        String[] workoutNames = {((Integer)workouts.getWorkouts().size()).toString(),"hey"};
 
-        //I'm having trouble accessing the workoutlab arraylist, this works with the test array though
-        /*for(int i = 0; i < workouts.getWorkouts().size(); i++) {
-            workoutNames[i] = workouts.getWorkouts().get(i).getWorkoutTitle();
-        }*/
-        String[] test = {"this", "tests","the", "dynamic", "listview"};
+        String[] workoutNames = new String[workouts.size()];
 
-        //to see that the mWorkouts size is zero, change this to return workoutNames and see the 0 in the listview
-        return test;
+        for(int i = 0; i < workouts.size(); i++) {
+            workoutNames[i] = workouts.get(i).getWorkoutTitle();
+        }
+
+        return workoutNames;
     }
 }
