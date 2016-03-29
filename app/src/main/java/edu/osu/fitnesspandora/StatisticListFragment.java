2af97d807f2b83f95f26c9433c573f40a48641be
Kyle Justice <kyle_justice@yahoo.com>
2016-03-28 23:11:21 -0400

    Making statistics page better.

--- app/src/main/java/edu/osu/fitnesspandora/StatisticListFragment.java
+++ app/src/main/java/edu/osu/fitnesspandora/StatisticListFragment.java
@@ -7,13 +7,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Locale;
 
@@ -28,7 +32,11 @@ public class StatisticListFragment extends ListFragment {
     // Initialize data from Firebase
     private WorkoutLab mWorkoutLab;
     private ExerciseLab mExerciseLab;
+    private ExerciseLogLab mExerciseLogLab;
     private ArrayList<ExerciseLog> mExerciseLogs;
+
+    private Button mDateButton;
+    private Button mScoreButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,15 +46,49 @@ public class StatisticListFragment extends ListFragment {
         // Initialize the user's credentials
         mUser = User.get(getActivity());
 
+        // Set the sort buttons to visible and hook em up
+        getActivity().findViewById(R.id.stats_sort_buttons).setVisibility(View.VISIBLE);
+        mDateButton = (Button) getActivity().findViewById(R.id.stats_date_button);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sort the exercise logs with the first being the most recent
+                Collections.sort(mExerciseLogs, new CompareExerciseDates());
+                // Restart the adapter
+                ExerciseLogAdapter adapter = new ExerciseLogAdapter(mExerciseLogs);
+                setListAdapter(adapter);
+            }
+        });
+        mScoreButton = (Button) getActivity().findViewById(R.id.stats_score_button);
+        mScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sort the exercise logs starting with the highest scored
+                Collections.sort(mExerciseLogs, new CompareExerciseScores());
+                // Restart the adapter
+                ExerciseLogAdapter adapter = new ExerciseLogAdapter(mExerciseLogs);
+                setListAdapter(adapter);
+            }
+        });
+
         // Initialize data from Firebase
         mWorkoutLab = WorkoutLab.get();
         mExerciseLab = ExerciseLab.get();
-        mExerciseLogs = ExerciseLogLab.get(mUser.getAuthUID()).getExerciseLogs();
+        mExerciseLogLab = ExerciseLogLab.get(mUser.getAuthUID());
+        mExerciseLogs = mExerciseLogLab.getExerciseLogs();
 
         // Discard the 'Skips', i.e the exercises the user didn't complete
         discardExerciseLogs();
 
-        ExerciseLogAdapter adapter = new ExerciseLogAdapter(mExerciseLogs);
+        // By defualt, sort the exercise logs with the first being the most recent
+        Collections.sort(mExerciseLogs, new CompareExerciseDates());
+
+        ExerciseLogConsolidatedLab elcLab = new ExerciseLogConsolidatedLab(mExerciseLogLab);
+
+        // Sort by the consolidated exercise scores
+        Collections.sort(elcLab.mConsExerciseLogs, new CompareConsolidatedExerciseScores());
+
+        ConsolidatedExerciseLogAdapter adapter = new ConsolidatedExerciseLogAdapter(elcLab.mConsExerciseLogs);
         setListAdapter(adapter);
     }
 
@@ -69,7 +111,7 @@ public class StatisticListFragment extends ListFragment {
     @Override
     public void onResume() {
         super.onResume();
-        ((ExerciseLogAdapter)getListAdapter()).notifyDataSetChanged();
+        ((ConsolidatedExerciseLogAdapter)getListAdapter()).notifyDataSetChanged();
     }
 
     private class ExerciseLogAdapter extends ArrayAdapter<ExerciseLog> {
@@ -106,10 +148,174 @@ public class StatisticListFragment extends ListFragment {
             dateFormat += theCal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()) + " ";
             dateFormat += theCal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) + ", ";
             dateFormat += theCal.get(Calendar.DAY_OF_MONTH) + ", ";
-            dateFormat += theCal.get(Calendar.YEAR);
+            dateFormat += theCal.get(Calendar.YEAR) + " @ ";
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+            dateFormat += sdf.format(theDate) + " ";
+            dateFormat += theCal.getDisplayName(Calendar.AM_PM, Calendar.LONG, Locale.getDefault());
             date.setText(dateFormat);
 
             return convertView;
         }
     }
+
+    private class ConsolidatedExerciseLogAdapter extends ArrayAdapter<ExerciseLogConsolidated> {
+
+        public ConsolidatedExerciseLogAdapter(ArrayList<ExerciseLogConsolidated> exerciseLogs) {
+            super(getActivity(), 0, exerciseLogs);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // If we weren't given a view, inflate one
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_exercise_log, null);
+            }
+            // Configure the view for this Workout
+            ExerciseLogConsolidated e = getItem(position);
+
+            Exercise exercise = mExerciseLab.getExercise(e.getExerciseID());
+            Workout workout = mWorkoutLab.getWorkout(e.getWorkoutID());
+
+            TextView exerciseTitle = (TextView) convertView.findViewById(R.id.exercise_log_list_item_exercisetitle);
+            exerciseTitle.setText(exercise.getExerciseTitle() + "Num likes: " + e.mSumLikes);
+
+            TextView workoutTitle = (TextView) convertView.findViewById(R.id.exercise_log_list_item_workouttitle);
+            workoutTitle.setText(workout.getWorkoutTitle() + " Workout");
+
+            TextView date = (TextView) convertView.findViewById(R.id.exercise_log_list_item_date);
+            Date theDate = new Date(e.getExerciseDate());
+            Calendar theCal = Calendar.getInstance();
+            theCal.setTime(theDate);
+
+            String dateFormat = "";
+            dateFormat += theCal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()) + " ";
+            dateFormat += theCal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) + ", ";
+            dateFormat += theCal.get(Calendar.DAY_OF_MONTH) + ", ";
+            dateFormat += theCal.get(Calendar.YEAR) + " @ ";
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+            dateFormat += sdf.format(theDate) + " ";
+            dateFormat += theCal.getDisplayName(Calendar.AM_PM, Calendar.LONG, Locale.getDefault());
+            date.setText(dateFormat);
+
+            return convertView;
+        }
+    }
+
+    public class CompareExerciseScores implements Comparator<ExerciseLog> {
+        @Override
+        public int compare(ExerciseLog e1, ExerciseLog e2) {
+
+            int result =  Integer.parseInt(Long.toString(mExerciseLogLab.exerciseScore(e2.getExerciseID()) - mExerciseLogLab.exerciseScore(e1.getExerciseID())));
+            // Tie breaker is the id of the exercise
+            if(result == 0){
+                result = Integer.parseInt(Long.toString(e2.getExerciseID() - e1.getExerciseID()));
+            }
+            return result;
+        }
+    }
+
+    public class CompareExerciseDates implements Comparator<ExerciseLog> {
+        @Override
+        public int compare(ExerciseLog e1, ExerciseLog e2) {
+            return Integer.parseInt(Long.toString(e2.getExerciseDate() - e1.getExerciseDate()));
+        }
+    }
+
+
+    public class CompareConsolidatedExerciseScores implements Comparator<ExerciseLogConsolidated> {
+        @Override
+        public int compare(ExerciseLogConsolidated e1, ExerciseLogConsolidated e2) {
+
+            int result =  Integer.parseInt(Long.toString(mExerciseLogLab.exerciseScore(e2.getExerciseID()) - mExerciseLogLab.exerciseScore(e1.getExerciseID())));
+            // Tie breaker is the id of the exercise
+            if(result == 0){
+                result = Integer.parseInt(Long.toString(e2.getExerciseID() - e1.getExerciseID()));
+            }
+            return result;
+        }
+    }
+
+    public class CompareConsolidatedExerciseDates implements Comparator<ExerciseLogConsolidated> {
+        @Override
+        public int compare(ExerciseLogConsolidated e1, ExerciseLogConsolidated e2) {
+            return Integer.parseInt(Long.toString(e2.getExerciseDate() - e1.getExerciseDate()));
+        }
+    }
+
+    public class ExerciseLogConsolidatedLab{
+
+        private ArrayList<ExerciseLogConsolidated> mConsExerciseLogs;
+
+        public ExerciseLogConsolidatedLab(ExerciseLogLab exerciseLogLab){
+            mConsExerciseLogs = new ArrayList<ExerciseLogConsolidated>();
+
+            for(ExerciseLog el : exerciseLogLab.getExerciseLogs()){
+                this.addExerciseLog(el);
+            }
+
+        }
+
+        public void addExerciseLog(ExerciseLog el){
+            boolean foundExercise = false;
+            for(ExerciseLogConsolidated elc : mConsExerciseLogs){
+                if(elc.mExerciseID == el.getExerciseID()){
+                    foundExercise = true;
+                    // Add the exercise to the consolidation object
+                    elc.addSameExercise(el);
+                }
+            }
+
+            if(!foundExercise){
+                // Make a new consolidated object if the exercise id is not found
+                mConsExerciseLogs.add(new ExerciseLogConsolidated(el));
+            }
+
+
+        }
+
+    }
+
+    public class ExerciseLogConsolidated{
+
+        public long mExerciseID;
+        public long mWorkoutID;
+        public long mSumLikes;
+        public long mSumSkips;
+        public long mMostRecentDate;
+
+        public ExerciseLogConsolidated(ExerciseLog el){
+            this.mExerciseID = el.getExerciseID();
+            this.mWorkoutID = el.getWorkoutID();
+            this.mSumLikes = 0;
+            this.mSumSkips = 0;
+            this.addSameExercise(el);
+        }
+
+        public void addSameExercise(ExerciseLog el){
+            // Sum up the exercise score
+            if(el.getexerciseScore() > 0){
+                this.mSumLikes++;
+            }else{
+                this.mSumSkips++;
+            }
+            // If the date is more recent, store it
+            if(this.mMostRecentDate < el.getExerciseDate()){
+                this.mMostRecentDate = el.getExerciseDate();
+            }
+        }
+
+        public long getExerciseID(){
+            return mExerciseID;
+        }
+
+        public long getExerciseDate(){
+            return mMostRecentDate;
+        }
+
+        public long getWorkoutID(){
+            return mWorkoutID;
+        }
+    }
+
 }
