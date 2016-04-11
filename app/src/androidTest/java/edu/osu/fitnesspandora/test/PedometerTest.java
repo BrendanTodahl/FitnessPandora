53fe126cf1e164d126e6584d3cd8ca157e1a92af
Brendan Todahl <Btodahl29@gmail.com>
2016-04-11 14:10:53 -0400

    Set up robotium testing framework. Added a number of testing classes that step through all of the app.

---
+++ app/src/androidTest/java/edu/osu/fitnesspandora/test/PedometerTest.java
@@ -0,0 +1,119 @@
+package edu.osu.fitnesspandora.test;
+
+import edu.osu.fitnesspandora.LoginActivity;
+import edu.osu.fitnesspandora.MainActivity;
+import edu.osu.fitnesspandora.PedometerActivity;
+import edu.osu.fitnesspandora.R;
+
+import com.robotium.solo.*;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+
+/**
+ * Created by Brendan on 4/10/2016.
+ */
+public class PedometerTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+  	private Solo solo;
+  	
+  	public PedometerTest() {
+		super(LoginActivity.class);
+  	}
+
+  	public void setUp() throws Exception {
+        super.setUp();
+		solo = new Solo(getInstrumentation());
+		getActivity();
+  	}
+  
+   	@Override
+   	public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+  	}
+  
+	public void testA_Login() {
+        //Wait for activity: 'edu.osu.fitnesspandora.LoginActivity'
+		solo.waitForActivity(LoginActivity.class, 2000);
+        //Enter the text: 'b@g.com'
+		solo.clearEditText((android.widget.EditText) solo.getView(R.id.email));
+		solo.enterText((android.widget.EditText) solo.getView(R.id.email), "b@g.com");
+        //Click on Empty Text View
+		solo.clickOnView(solo.getView(R.id.password));
+        //Enter the text: 'password'
+		solo.clearEditText((android.widget.EditText) solo.getView(R.id.password));
+		solo.enterText((android.widget.EditText) solo.getView(R.id.password), "password");
+        //Click on Login
+		solo.clickOnView(solo.getView(R.id.email_sign_in_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+		assertTrue("edu.osu.fitnesspandora.MainActivity is not found!", solo.waitForActivity(MainActivity.class));
+	}
+
+	public void testB_NavigateToPedometer() {
+		//Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+		solo.waitForActivity(MainActivity.class, 2000);
+		//Click on Pedometer
+		solo.clickOnView(solo.getView(R.id.pedemeter_button));
+		//Wait for activity: 'edu.osu.fitnesspandora.PedometerActivity'
+		assertTrue("edu.osu.fitnesspandora.PedometerActivity is not found!", solo.waitForActivity(PedometerActivity.class));
+	}
+
+	public void testC_RunPedometer() {
+		//Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+		solo.waitForActivity(MainActivity.class, 2000);
+		//Click on Pedometer
+		solo.clickOnView(solo.getView(R.id.pedemeter_button));
+		//Wait for activity: 'edu.osu.fitnesspandora.PedometerActivity'
+		assertTrue("edu.osu.fitnesspandora.PedometerActivity is not found!", solo.waitForActivity(PedometerActivity.class));
+		//Click on Start
+		solo.clickOnView(solo.getView(R.id.startStopPedoButton));
+		//Change text to emulate a 'step'
+		final TextView mStepsTaken = (TextView) solo.getView(R.id.pedometerCounter);
+		solo.getCurrentActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				mStepsTaken.setText("1");
+			}
+		});
+		solo.waitForCondition(new Condition() {
+			@Override
+			public boolean isSatisfied() {
+				return Integer.parseInt(mStepsTaken.getText().toString()) == 1;
+			}
+		}, 2000);
+		assertTrue("edu.osu.fitnesspandora.PedometerActivity did not take any steps!", Integer.parseInt(mStepsTaken.getText().toString()) != 0);
+		//Click on Stop
+		solo.clickOnView(solo.getView(R.id.startStopPedoButton));
+		//Click on Reset
+		solo.clickOnView(solo.getView(R.id.resetPedoButton));
+		solo.waitForCondition(new Condition() {
+			@Override
+			public boolean isSatisfied() {
+				return Integer.parseInt(mStepsTaken.getText().toString()) == 0;
+			}
+		}, 2000);
+		assertTrue("edu.osu.fitnesspandora.PedometerActivity did not reset step counter!", Integer.parseInt(mStepsTaken.getText().toString()) == 0);
+	}
+
+	public void testD_NavigateBackToMain() {
+		//Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+		solo.waitForActivity(MainActivity.class, 2000);
+		//Click on Pedometer
+		solo.clickOnView(solo.getView(R.id.pedemeter_button));
+		//Wait for activity: 'edu.osu.fitnesspandora.PedometerActivity'
+		assertTrue("edu.osu.fitnesspandora.PedometerActivity is not found!", solo.waitForActivity(PedometerActivity.class));
+		//Press menu back key
+		solo.goBack();
+		assertTrue("edu.osu.fitnesspandora.MainActivity is not found!", solo.waitForActivity(MainActivity.class));
+	}
+
+	public void testE_Logout() {
+		//Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+		solo.waitForActivity(MainActivity.class, 2000);
+		//Click on ImageView
+		solo.clickOnView(solo.getView(R.id.logout_button));
+		//Wait for activity: 'edu.osu.fitnesspandora.LoginActivity'
+		assertTrue("edu.osu.fitnesspandora.LoginActivity is not found!", solo.waitForActivity(LoginActivity.class));
+	}
+}
---
+++ app/src/androidTest/java/edu/osu/fitnesspandora/test/WorkoutTest.java
@@ -0,0 +1,208 @@
+package edu.osu.fitnesspandora.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import edu.osu.fitnesspandora.LoginActivity;
+import edu.osu.fitnesspandora.MainActivity;
+import edu.osu.fitnesspandora.R;
+import edu.osu.fitnesspandora.WorkoutActivity;
+import edu.osu.fitnesspandora.WorkoutListActivity;
+
+/**
+ * Created by Brendan on 4/11/2016.
+ */
+public class WorkoutTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+    private Solo solo;
+
+    public WorkoutTest() {
+        super(LoginActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation());
+        getActivity();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+
+    public void testA_Login() {
+        //Wait for activity: 'edu.osu.fitnesspandora.LoginActivity'
+        solo.waitForActivity(LoginActivity.class, 2000);
+        //Enter the text: 'b@g.com'
+        solo.clearEditText((android.widget.EditText) solo.getView(R.id.email));
+        solo.enterText((android.widget.EditText) solo.getView(R.id.email), "b@g.com");
+        //Click on Empty Text View
+        solo.clickOnView(solo.getView(R.id.password));
+        //Enter the text: 'password'
+        solo.clearEditText((android.widget.EditText) solo.getView(R.id.password));
+        solo.enterText((android.widget.EditText) solo.getView(R.id.password), "password");
+        //Click on Login
+        solo.clickOnView(solo.getView(R.id.email_sign_in_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        assertTrue("edu.osu.fitnesspandora.MainActivity is not found!", solo.waitForActivity(MainActivity.class));
+    }
+
+    public void testB_NavigateToWorkout() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+    }
+
+    public void testC_SelectArms() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Arms
+        solo.clickOnMenuItem("Arms");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testD_SelectCardio() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Cardio
+        solo.clickOnMenuItem("Cardio");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testE_SelectCore() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Core
+        solo.clickOnMenuItem("Core");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testF_SelectFullBody() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Full Body
+        solo.clickOnMenuItem("Full Body");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testG_SelectLowerBody() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Lower Body
+        solo.clickOnMenuItem("Lower Body");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testH_SelectUpperBody() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Upper Body
+        solo.clickOnMenuItem("Upper Body");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testI_InstructionalVideoArms() throws InterruptedException {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Arms
+        solo.clickOnMenuItem("Arms");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+        //Click on Instructional Video
+        solo.clickOnView(solo.getView(R.id.exercise_instructions));
+        //Wait for URL to load video for 8 seconds
+        Thread.sleep(8000);
+        //Go Back
+        solo.goBack();
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testJ_EnterRepsAndWeight() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Arms
+        solo.clickOnMenuItem("Arms");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+        //Enter the text: '10'
+        solo.clearEditText((android.widget.EditText) solo.getView(R.id.editReps));
+        solo.enterText((android.widget.EditText) solo.getView(R.id.editReps), "10");
+        //Enter the text: '45'
+        solo.clearEditText((android.widget.EditText) solo.getView(R.id.editWeight));
+        solo.enterText((android.widget.EditText) solo.getView(R.id.editWeight), "45");
+        //'Like' the workout
+        solo.clickOnView(solo.getView(R.id.button_like));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testK_SkipWorkout() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on Workout
+        solo.clickOnView(solo.getView(R.id.workout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutListActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutListActivity is not found!", solo.waitForActivity(WorkoutListActivity.class));
+        //Click on Arms
+        solo.clickOnMenuItem("Arms");
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+        //'Skip' the workout
+        solo.clickOnView(solo.getView(R.id.button_skip));
+        //Wait for activity: 'edu.osu.fitnesspandora.WorkoutActivity'
+        assertTrue("edu.osu.fitnesspandora.WorkoutActivity is not found!", solo.waitForActivity(WorkoutActivity.class));
+    }
+
+    public void testL_Logout() {
+        //Wait for activity: 'edu.osu.fitnesspandora.MainActivity'
+        solo.waitForActivity(MainActivity.class, 2000);
+        //Click on ImageView
+        solo.clickOnView(solo.getView(R.id.logout_button));
+        //Wait for activity: 'edu.osu.fitnesspandora.LoginActivity'
+        assertTrue("edu.osu.fitnesspandora.LoginActivity is not found!", solo.waitForActivity(LoginActivity.class));
+    }
+}
